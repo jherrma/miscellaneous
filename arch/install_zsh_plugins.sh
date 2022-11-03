d9ccaa8c867088bd1063f3330125f36f053c95f9
jon-ryan <37742289+jon-ryan@users.noreply.github.com>
2022-11-03 16:01:27 +0100

    Create install_zsh_plugins.sh

---
+++ arch/install_zsh_plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+echo 'source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
