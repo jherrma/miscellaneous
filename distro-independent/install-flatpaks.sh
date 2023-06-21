2ea5d1e009219eb91de49e0bc0d598dca102b964
Jonathan Herrmann <jonorh@web.de>
2023-06-21 19:54:46 +0200

    add snap and flatpak installation scripts

---
+++ distro-independent/install-flatpaks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo flatpak install flathub com.brave.Browser
+sudo flatpak install flathub com.github.tchx84.Flatseal
+sudo flatpak install flathub im.riot.Riot
+sudo flatpak install flathub org.gnome.World.Secrets
+sudo flatpak install flathub org.onlyoffice.desktopeditors
+sudo flatpak install flathub org.videolan.VLC
